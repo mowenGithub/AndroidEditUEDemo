e25d8cf50997793056736862079a54d5676f6d12
wenlong.mo <1245146613@qq.com>
2015-12-14 00:01:12 +0800

    Done Demo

--- app/src/main/java/com/mowen/androidedituedemo/MainActivity.java
+++ app/src/main/java/com/mowen/androidedituedemo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.mowen.androidedituedemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
+
+        EditText etContent = (EditText) findViewById(R.id.etContent);
+//        etContent.setImeOptions(EditorInfo.);
+        etContent.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
